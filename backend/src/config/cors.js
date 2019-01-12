cc0327d8522ae4a683e8b6ddc1e3f336917007f4
Leonardo Dias <leo-rock14@hotmail.com>
2019-01-12 19:28:11 -0200

    ajustes no cors

--- backend/src/config/cors.js
+++ backend/src/config/cors.js
@@ -1,8 +1,8 @@
 module.exports = (req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
-    "Access-Control-Allow-Metohds",
-    "GET, POST, PUT, DELETE, OPTIONS, PATCH"
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
   );
   res.header(
     "Access-Control-Allow-Headers",
